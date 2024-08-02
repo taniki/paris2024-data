53f40dfa20000d381fdcdfc8a86c5556567e3149
duong tam kien <tk@renard.ninja>
2024-08-02 20:24:13 +0200

    ajoute un script qui permet de récupérer le détail des médailles

---
+++ recipes/prep_medallist.py
@@ -0,0 +1,132 @@
+# +
+import pandas as pd
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import math
+
+source = 'https://olympics.com/en/paris-2024/medals/medallists'
+
+def get_total(row):
+    # print(row)
+    spans = row.find_all('span')
+    
+    if (len(spans) == 0):
+        return
+    
+    country = spans[0].text.strip()
+    name = spans[1].text.strip()
+    
+    gold = int(spans[2].text.strip())
+    silver = int(spans[3].text.strip())
+    bronze = int(spans[4].text.strip())
+
+    return {
+        # 'code': spans[1].text.strip(),
+        'code': country,
+        'name': name,
+        'gold': gold,
+        'silver': silver,
+        'bronze': bronze
+    }
+
+
+def parse_medal_row(row):
+    # print(row)
+    a = row.find_all('a')
+    spans = row.find_all('span')
+    # print(a)
+    # print(spans)
+    
+    try:
+        discipline = a[0].text.strip()
+        event = a[1].text.strip()
+        color = spans[0].text.strip()
+
+        return {
+            'discipline': discipline,
+            'event': event,
+            'color': color
+        }
+    except:
+        return
+
+
+def get_medals(row):
+    # print(row)
+    spans = row.find_all('span')
+    
+    if (len(spans) == 0):
+        return
+    
+    country = spans[0].text.strip()
+    name = spans[1].text.strip()
+    medals = [ parse_medal_row(row) for row in row.find_all('div', { 'data-medal-detail-id' : True })]
+    
+    return {
+        # 'code': spans[1].text.strip(),
+        'code': country,
+        'name': name,
+        'medals': medals
+    }
+
+def get_rows(page_source):
+    soup = BeautifulSoup(page_source, "html.parser")
+    rows = soup.find_all('div', {'data-index': True })
+    return [get_total(row) for row in rows]
+
+def get_rows2(page_source):
+    soup = BeautifulSoup(page_source, "html.parser")
+    rows = soup.find_all('div', {'data-index': True })
+    return [get_medals(row) for row in rows]
+
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=True)  # Change to True for headless mode
+    page = browser.new_page()
+    page.goto(source)
+    
+    page.click('button[title="Expand all rows"]')
+    
+    # Get the initial scroll height
+    height = page.evaluate("document.documentElement.scrollHeight")
+    window_height = page.evaluate("window.innerHeight * 1/2")
+    
+    medallists_total = []
+    medals = []
+
+    page.evaluate("window.scrollTo(0, 0)")
+    
+    for y in range(0, math.ceil(height / window_height)):
+        page.evaluate(f"window.scrollTo(0, {y*window_height})")
+        #page.wait_for_timeout(1000)  # Give time for the page to load
+        medallists_total += get_rows(page.content())
+        medals += get_rows2(page.content())
+
+    #print(medals)
+        
+    browser.close()
+
+medallist_df = (
+    pd.DataFrame
+    .from_records([ x for x in medallists_total if x != None ])
+    .drop_duplicates()
+)
+
+print(medallist_df)
+
+medallist_df.to_csv('../datasets/medallists.csv', index=False)
+
+
+medals_df = (
+    pd.DataFrame
+    .from_records([ x for x in medals if x != None ])
+    .explode('medals')
+    .pipe(lambda df:
+        df.join(df.medals.apply(pd.Series))
+    )
+    .drop('medals', axis=1)
+    .drop_duplicates()
+)
+
+print(medals_df)
+
+medals_df.to_csv('../datasets/medals.csv', index=False)
